6bef8c905b1cbb0a8d838fda67e6586e009e7d77
Shane Vandegrift <vandesm14@gmail.com>
2021-12-04 15:49:16 -0500

    for later

---
+++ stupid/client/BfChip.cs
@@ -0,0 +1,42 @@
+// TODO cleanup unneeded references
+using System.Collections.Generic;
+using System.Text;
+using JimmysUnityUtilities;
+using JimmysUnityUtilities.Random;
+using LogicLocalization;
+using LogicUI;
+using LogicWorld.ClientCode;
+using LogicWorld.ClientCode.LabelAlignment;
+using LogicWorld.ClientCode.Resizing;
+using LogicWorld.Interfaces;
+using LogicWorld.References;
+using LogicWorld.Rendering.Chunks;
+using LogicWorld.Rendering.Components;
+using LogicWorld.SharedCode.Components;
+using TMPro;
+using UnityEngine;
+
+namespace FileEmitterMod.Client
+{
+    class FEM01 : ComponentClientCode<Label.IData>
+    {
+        private const string PLACEHOLDER_PROGRAM = "program here";
+
+        protected static Color24 DefaultColor 
+			= new Color24((byte) 38, (byte) 38, (byte) 38);
+
+        protected override void SetDataDefaultValues()
+        {
+            Data.LabelText = PLACEHOLDER_PROGRAM;
+
+            // The rest of the IData properties aren't used by the mod
+            Data.LabelFontSizeMax = 0f;
+            Data.LabelColor = DefaultColor;
+            Data.LabelMonospace = false;
+            Data.HorizontalAlignment = LabelAlignmentHorizontal.Center;
+            Data.VerticalAlignment = LabelAlignmentVertical.Middle;
+            Data.SizeX = 1;
+            Data.SizeZ = 1;
+        }
+    }
+}
---
+++ stupid/client/Loader.cs
@@ -0,0 +1,7 @@
+using LICC;
+using LogicAPI.Client;
+
+namespace SmMod
+{
+    class SmClientMod : ClientMod {}
+}
