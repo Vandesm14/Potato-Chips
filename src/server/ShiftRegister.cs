0e081679269ae0f215fd0753107843743e85695e
Shane Vandegrift <vandesm14@gmail.com>
2021-12-04 20:29:29 -0500

    stuff

---
+++ src/server/ShiftRegister.cs
@@ -0,0 +1,65 @@
+using LogicAPI.Server.Components;
+using LogicLog;
+using System;
+
+namespace sv {
+  public class ShiftRegister : LogicComponent {
+    private bool _clk {
+      get {
+        return base.Inputs[0].On;
+      }
+    }
+
+    private bool _data {
+      get {
+        return base.Inputs[1].On;
+      }
+    }
+
+    private bool _out0 {
+      set {
+        base.Outputs[0].On = value;
+      }
+    }
+
+    private bool _out1 {
+      set {
+        base.Outputs[1].On = value;
+      }
+    }
+
+    private bool _out2 {
+      set {
+        base.Outputs[2].On = value;
+      }
+    }
+
+    private bool _out3 {
+      set {
+        base.Outputs[3].On = value;
+      }
+    }
+
+    private int _out {
+      set {
+        _out0 = Convert.ToBoolean((value & 0b1000) >> 3);
+        _out1 = Convert.ToBoolean((value & 0b0100) >> 2);
+        _out2 = Convert.ToBoolean((value & 0b0010) >> 1);
+        _out3 = Convert.ToBoolean(value & 0b0001);
+      }
+    }
+
+    private bool[] mem = new bool[4];
+
+    protected override void DoLogicUpdate() {
+      if (_clk) {
+        mem[0] = mem[1];
+        mem[1] = mem[2];
+        mem[2] = mem[3];
+        mem[3] = _data;
+      }
+
+      _out = (mem[0] ? 8 : 0) + (mem[1] ? 4 : 0) + (mem[2] ? 2 : 0) + (mem[3] ? 1 : 0);
+    }
+  }
+}
---
+++ src/server/StackChip.cs
@@ -0,0 +1,58 @@
+using LogicAPI.Server.Components;
+using LogicLog;
+
+namespace sv {
+  public class StackChip : LogicComponent {
+    private bool _prev {
+      get {
+        return base.Inputs[0].On;
+      }
+    }
+
+    private bool _next {
+      get {
+        return base.Inputs[1].On;
+      }
+    }
+
+    private bool _write {
+      get {
+        return base.Inputs[2].On;
+      }
+    }
+
+    private bool _data {
+      get {
+        return base.Inputs[3].On;
+      }
+    }
+
+    private bool _out {
+      set {
+        base.Outputs[0].On = value;
+      }
+    }
+
+    private bool[] mem = new bool[256];
+    private int ptr = 0;
+
+    protected override void DoLogicUpdate() {
+      if (_write) {
+        mem[ptr] = _data;
+      }
+
+      if (_prev && _next) {
+        ptr = 0;
+      } else if (_next) {
+        ptr++;
+        ptr = ptr % 256;
+      } else if (_prev) {
+        ptr--;
+        if (ptr < 0) {
+          ptr = 256 - 1;
+        }
+      }
+      _out = mem[ptr];
+    }
+  }
+}
